123c973ca506bbd4fd2fbd99ecec35cd37e69776
Pranay <pranayraut79@gmail.com>
2026-02-16 23:53:14 +0530

    Requirement 3:
    Created AuthTokenProvider - Manage the token for different roles
    
    Role: Hold all the Roles of the Phoenix Application
    
    UserDetailsAPITest : Functional API Test checks the status code,
    response time and Schema of the UserDetails API
    
    CountAPITest : Functional API Test checks the status code, response time
    and Schema of the Count API. Also does the token testing for negative
    scenarios
    
    MasterAPITest: Functional API Test checks the status code, response time and Schema of the Master API. Also does the token testing for negative scenarios

---
+++ src/test/java/com/api/tests/MasterAPITest.java
@@ -0,0 +1,64 @@
+package com.api.tests;
+
+import static com.api.constants.Role.FD;
+import static com.api.utils.AuthTokenProvider.getToken;
+import static com.api.utils.ConfigManager.getProperty;
+import static io.restassured.RestAssured.given;
+
+import java.io.IOException;
+
+
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+public class MasterAPITest {
+	
+	@Test
+	public void masterAPITest() throws IOException  {
+		given()
+		.baseUri(getProperty("BASE_URI"))
+		.and()
+		.header("Authorization", getToken(FD))
+		.and()
+		.contentType("")
+		.log().all()
+		.when()
+		.post("master")// default content type added by RA is application/url-formencoded
+		.then()
+		.log().all()
+		.statusCode(200)
+		.time(lessThan(1000L))
+		.body("message", equalTo("Success"))
+		.body("data", notNullValue())
+		.body("data", hasKey("mst_oem"))
+		.body("data", hasKey("mst_model"))
+		.body("$", hasKey("message"))
+		.body("$", hasKey("data"))
+		.body("data.mst_oem.size()", equalTo(2))// check size of json array for mst_oem
+		.body("data.mst_model.size()", greaterThan(0))
+		.body("data.mst_oem.id", everyItem(notNullValue()))
+		.body("data.mst_oem.name", everyItem(notNullValue()))
+		.body(matchesJsonSchemaInClasspath("response-schema/MasterAPIResponseSchema.json"));
+		
+	}
+	
+	@Test
+	public void invaliTokenMasterAPITest() throws IOException {
+		given()
+		.baseUri(getProperty("BASE_URI"))
+		.and()
+		.header("Authorization", "")
+		.and()
+		.contentType("")
+		.log().all()
+		.when()
+		.post("master")// default content type added by RA is application/url-formencoded
+		.then()
+		.log().all()
+		.statusCode(401);
+		
+	}
+
+}
