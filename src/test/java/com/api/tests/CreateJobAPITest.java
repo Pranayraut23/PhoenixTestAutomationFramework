9d775d396ec00b6806e396a365562e084e0ea4e9
Pranay <pranayraut79@gmail.com>
2026-03-15 16:58:29 +0530

    Updated the framwork to java 16
    Used the Record Feature of Java and converted all the POJO classes to
    records in the com.api.pojo

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -1,5 +1,12 @@
 package com.api.tests;
 
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import com.api.constants.Role;
@@ -8,14 +15,9 @@ import com.api.pojo.Customer;
 import com.api.pojo.CustomerAddress;
 import com.api.pojo.CustomerProduct;
 import com.api.pojo.Problems;
-import com.api.utils.AuthTokenProvider;
-import com.api.utils.ConfigManager;
 import com.api.utils.SpecUtil;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-
-import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class CreateJobAPITest {
 	
@@ -30,20 +32,24 @@ public class CreateJobAPITest {
 
 		CustomerAddress customerAddress = new CustomerAddress("A 404 ", "GB Nagar ", "GG Road", "Inorbit", "Mumbai", "411002", "India", "Maharashtra"); 
 
-		CustomerProduct customerProduct = new CustomerProduct("2025-04-06T18:30:00.000Z", "98053710550731", "98053710550731", "98053710550731", "2025-04-06T18:30:00.000Z", 1, 1);
+		CustomerProduct customerProduct = new CustomerProduct("2025-04-06T18:30:00.000Z", "11053710550731", "11053710550731", "11053710550731", "2025-04-06T18:30:00.000Z", 1, 1);
 				
 		Problems problems = new Problems(1, "Battery Issue") ;
 		
-		Problems[] problemArray = new Problems[1];
-		problemArray[0] = problems;
-		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemArray);
+		List<Problems> problemList = new ArrayList<Problems>();
+		problemList.add(problems);
+		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemList);
 		
 		
 		given()
 		.spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload))
 		.when().post("/job/create")
 		.then()
-		.spec(SpecUtil.responseSpec_OK());
+		.spec(SpecUtil.responseSpec_OK())
+		.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
+		.body("message", equalTo("Job created successfully. "))
+		.body("data.mst_service_location_id", equalTo(1))
+		.body("data.job_number", startsWith("JOB_"));
 
 	}
 
